39b35039ca8463202466a946872d941704226dfc
Ondra Kučera <ondrej.kucera@centrum.cz>
2023-10-22 16:50:05 +0200

    Add basic project structure
    As generated by Spring Initializr.

---
+++ src/main/java/ondra/students/StudentsServerApplication.java
@@ -0,0 +1,13 @@
+package ondra.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ondra/students/StudentsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ondra.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
